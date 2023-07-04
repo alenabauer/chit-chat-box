cb5006e0b862d72fd3cafd2571187596b52f3d1b
Alena Bauer <alena.bauer@kleerly.de>
2023-07-04 14:11:38 +0200

    wip: create an answer-matching service

--- services/answer-matching/main.py
+++ services/answer-matching/main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+app = FastAPI()
+
 class Question(BaseModel):
     question: str
 
-app = FastAPI()
-
-@app.post("/ask")
-def ask(question: Question):
+@app.post("/match")
+def find_best_matching_answer(question: Question):
     # retrieve the question from the request payload
     user_question = question.question
 
-    # TODO: connect to the answer-matching service and retrieve the best matching answer
+    # TODO: find the best matching answer from the knowledge base
+    best_matching_answer = "This is the best matching answer."
 
     # create a JSON response with the best matching answer
-    response = {"answer": "This is the best matching answer."}
+    response = {"answer": best_matching_answer}
 
     return response
